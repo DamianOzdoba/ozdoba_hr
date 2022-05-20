70c44a69e10cd9787f469de01a3e52987a690048
damian <damian@iMac.local>
2022-05-20 13:22:48 +0200

    code refactor

--- src/js/components/Employees.js
+++ src/js/components/Employees.js
@@ -4,9 +4,9 @@ import offers_bg_white from "../../images/offers_bg_white.png";
 
 function Employees() {
     return (
-        <div className="employees" style={{backgroundImage: `url(${offers_bg_white})`}}>
-            <div className="employees__offers">
-                <h2>Aktualnie otwarte rekrutacje:</h2>
+        <div className="employ" style={{backgroundImage: `url(${offers_bg_white})`}}>
+            <div className="employ__offers">
+                <h2>AKLUALNIE OTWARTE REKRUTACJE:</h2>
                 <Offers />
             </div>
         </div>
--- src/js/components/Employers.js
+++ src/js/components/Employers.js
@@ -4,9 +4,9 @@ import offers_bg_white from "../../images/offers_bg_white.png";
 
 function Employers() {
     return (
-        <div className="employers" style={{backgroundImage: `url(${offers_bg_white})`}}>
-            <div className="employers__cooperation-offers">
-                <h2>Modele współpracy:</h2>
+        <div className="employ" style={{backgroundImage: `url(${offers_bg_white})`}}>
+            <div className="employ__offers">
+                <h2>MODELE WSPÓŁPRACY:</h2>
                 <div className="cooperation-offers">
                     <CooperationOffer/>
                 </div>
